fd1fb41f8fe70252f5aa4ef61eab761f4c286761
Zergoz <kristoffer.m.overgaard@gmail.com>
2023-09-20 11:46:21 +0200

    WorkSheet 2 done

--- WorkSheet_2/w2p4.js
+++ WorkSheet_2/w2p4.js
@@ -11,7 +11,7 @@ window.onload = function init()
     // Variables (somewhat primitive)
     var max_verts = 1000;
     var index = 0; var numTriangles = 0; var start = [ 0 ];
-    var drawMode = "point"; triangleCounter = 0;
+    var drawMode = "point"; counter = 0;
     var record = [ 0 ]; crecord = [ 0 ];
 
     var clickBuffer = gl.createBuffer();
@@ -53,19 +53,31 @@ window.onload = function init()
 
     var pointButton = document.getElementById("PointButton");
     pointButton.addEventListener("click", function(ev) {
-        drawMode = "point";
+        if (drawMode != "point") {
+            drawMode = "point";
+            counter = 0;
+        }
     });
-
+    
     var triangleButton = document.getElementById("TriangleButton");
     triangleButton.addEventListener("click", function(ev) {
-        drawMode = "triangle";
+        if (drawMode != "triangle") {
+            drawMode = "triangle";
+            counter = 0;
+        }
+    });
+    
+    var circleButton = document.getElementById("CircleButton");
+    circleButton.addEventListener("click", function(ev) {
+        if (drawMode != "circle") {
+            drawMode = "circle";
+            counter = 0;
+        }
     });
 
     canvas.addEventListener("click", function(ev) {
         switch (drawMode) {
             case "point":
-                triangleCounter = 0;
-
                 // Color stuff
                 gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
                 var t = flatten(vec4(colors[colorMenu.selectedIndex]));
@@ -93,13 +105,13 @@ window.onload = function init()
 
             case "triangle":
                 var bbox = ev.target.getBoundingClientRect();
-                if (triangleCounter < 2) {
-                    record[triangleCounter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
+                if (counter < 2) {
+                    record[counter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
 
                     // Color stuff
                     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
                     var t = flatten(vec4(colors[colorMenu.selectedIndex]));
-                    crecord[triangleCounter] = t;
+                    crecord[counter] = t;
                     for (var i = 0; i < 4; ++i) {
                         gl.bufferSubData(gl.ARRAY_BUFFER, (index+i)*sizeof['vec4'], t);
                     }
@@ -119,12 +131,12 @@ window.onload = function init()
                     start[numTriangles + 1] = index + 1;
                     index += 4;
                     numTriangles += 2;
-                    ++triangleCounter;
+                    ++counter;
                 }
                 else {
                     var t = flatten(vec4(colors[colorMenu.selectedIndex]));
-                    crecord[triangleCounter] = t;
-                    record[triangleCounter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
+                    crecord[counter] = t;
+                    record[counter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
                     index -= 8;
                     numTriangles -= 3;
                     start[numTriangles] = index;
@@ -138,9 +150,102 @@ window.onload = function init()
                     for (var i = 0; i < 3; ++i) {
                         gl.bufferSubData(gl.ARRAY_BUFFER, (index+i)*sizeof['vec2'], flatten(record[i]));
                     }
-                    triangleCounter = 0;
+                    counter = 0;
                     index += 3 ;
                 }
+                break;
+            case "circle":
+                var bbox = ev.target.getBoundingClientRect();
+                if (counter < 1) {
+                    record[counter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
+
+                    // Color stuff
+                    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+                    var t = flatten(vec4(colors[colorMenu.selectedIndex]));
+                    crecord[counter] = t;
+                    for (var i = 0; i < 4; ++i) {
+                        gl.bufferSubData(gl.ARRAY_BUFFER, (index+i)*sizeof['vec4'], t);
+                    }
+                    
+                    // Position stuff
+                    gl.bindBuffer(gl.ARRAY_BUFFER, clickBuffer);
+                    var tl = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.02, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 0.98);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec2'], flatten(tl));
+                    var tr = vec2(2*(ev.clientX - bbox.left)/canvas.width - 0.98, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 0.98);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index+1)*sizeof['vec2'], flatten(tr));
+                    var bl = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.02, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1.02);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index+2)*sizeof['vec2'], flatten(bl));
+                    var br = vec2(2*(ev.clientX - bbox.left)/canvas.width - 0.98, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1.02);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index+3)*sizeof['vec2'], flatten(br));
+                    
+                    start[numTriangles] = index;
+                    start[numTriangles + 1] = index + 1;
+                    index += 4;
+                    numTriangles += 2;
+                    ++counter;
+                }
+                else {
+                    var t = flatten(vec4(colors[colorMenu.selectedIndex]));
+                    crecord[counter] = t;
+                    record[counter] = vec2(2*(ev.clientX - bbox.left)/canvas.width - 1.0, 2*(canvas.height - ev.clientY + bbox.top - 1)/canvas.height - 1);
+                    
+                    var centerX = record[0][0];
+                    var centerY = record[0][1];
+                    var secondX = record[1][0];
+                    var secondY = record[1][1];
+                    var a = centerX - secondX;
+                    var b = centerY - secondY;
+                    var r = Math.sqrt((a*a) + (b*b));
+                    var n = 50;
+                    for (let i = 0; i <= n; ++i) {
+                        var theta = (2*Math.PI*i)/n;
+                        var perifey = vec2(r*Math.cos(theta) + centerX, r*Math.sin(theta) + centerY);
+                        record[i+1] = perifey;
+                    }
+                    
+                    index -= 4;
+                    numTriangles -= 2;
+                    
+                    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec4'], flatten(crecord[0]));
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index+1)*sizeof['vec4'], flatten(crecord[1]));
+
+
+                    gl.bindBuffer(gl.ARRAY_BUFFER, clickBuffer);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec2'], flatten(record[0]));
+                    gl.bufferSubData(gl.ARRAY_BUFFER, (index+1)*sizeof['vec2'], flatten(record[1]));
+                    
+                    index += 2;
+                    for (var i = 0; i <= n+1; ++i) {
+                        if (i <= n) {
+                            start[numTriangles] = index;
+                            ++numTriangles;
+                        }
+
+                        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+                        if (i % 2 == 0) {                            
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec4'], flatten(crecord[1]));
+                        }
+                        else {
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec4'], flatten(crecord[1]));
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index+1)*sizeof['vec4'], flatten(crecord[0]));
+                        }
+                        
+                        gl.bindBuffer(gl.ARRAY_BUFFER, clickBuffer);
+                        if (i % 2 == 0) {                            
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec2'], flatten(record[i]));
+                            index += 1;
+                        }
+                        else {
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index)*sizeof['vec2'], flatten(record[i]));
+                            gl.bufferSubData(gl.ARRAY_BUFFER, (index+1)*sizeof['vec2'], flatten(record[0]));
+                            index += 2;
+                        }
+                    }
+                    counter = 0;
+                    index++;
+                }
+
                 break;
         }
         render(gl, numTriangles, start);
