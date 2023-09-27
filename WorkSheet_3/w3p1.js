7e0e1c428547491f66616f89861647ff55c40248
Zergoz <kristoffer.m.overgaard@gmail.com>
2023-09-27 12:58:36 +0200

    Worksheet 3 done

---
+++ WorkSheet_3/w3p1.js
@@ -0,0 +1,90 @@
+window.onload = function init()
+{
+    var canvas = document.getElementById("webgl-canvas");
+    var gl = canvas.getContext("webgl");
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var numVertices = 36;
+    var T = translate(0.5, 0.5, 0.5);
+
+    var m = lookAt(vec3(0,0,0), vec3(0.5,0.5,-0.5), vec3(-0.5,0.5,-0.5));
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "view"), false, flatten(m));
+
+    var cubeColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ], // black
+        [ 1.0, 0.0, 0.0, 1.0 ], // red
+        [ 1.0, 1.0, 0.0, 1.0 ], // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ], // green
+        [ 0.0, 0.0, 1.0, 1.0 ], // blue
+        [ 1.0, 0.0, 1.0, 1.0 ], // magenta
+        [ 1.0, 1.0, 1.0, 1.0 ], // white
+        [ 0.0, 1.0, 1.0, 1.0 ] // cyan
+    ];
+
+    var cubeVertices = [
+        vec4(-0.5, -0.5, 0.5, 1),
+        vec4(-0.5, 0.5, 0.5, 1),
+        vec4(0.5, 0.5, 0.5, 1),
+        vec4(0.5, -0.5, 0.5, 1),
+        vec4(-0.5, -0.5, -0.5, 1),
+        vec4(-0.5, 0.5, -0.5, 1),
+        vec4(0.5, 0.5, -0.5, 1),
+        vec4(0.5, -0.5, -0.5, 1)
+    ];
+    
+    var triangleIndices = [
+        1, 0, 3,
+        3, 2, 1,
+        2, 3, 7,
+        7, 6, 2,
+        3, 0, 4,
+        4, 7, 3,
+        6, 5, 1,
+        1, 2, 6,
+        4, 5, 6,
+        6, 7, 4,
+        5, 4, 0,
+        0, 1, 5
+    ];
+
+    var wireIndices = new Uint32Array([
+        0, 1, 1, 2, 2, 3, 3, 0, // front
+        2, 3, 3, 7, 7, 6, 6, 2, // right
+        0, 3, 3, 7, 7, 4, 4, 0, // down
+        1, 2, 2, 6, 6, 5, 5, 1, // up
+        4, 5, 5, 6, 6, 7, 7, 4, // back
+        0, 1, 1, 5, 5, 4, 4, 0 // left
+    ]);
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cubeVertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "a_Position");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cubeColors), gl.STATIC_DRAW);
+
+    var cPosition = gl.getAttribLocation(program, "a_Color");
+    gl.vertexAttribPointer(cPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(cPosition);
+    
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);    
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(wireIndices), gl.STATIC_DRAW);
+
+    function render() {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawElements(gl.LINES, numVertices, gl.UNSIGNED_BYTE, 0);
+    }
+    
+    render();
+}   
---
+++ WorkSheet_3/w3p2.js
@@ -0,0 +1,89 @@
+window.onload = function init()
+{
+    var canvas = document.getElementById("webgl-canvas");
+    var gl = canvas.getContext("webgl");
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var numVertices = 36;
+    var T = translate(0.5, 0.5, 0.5);
+
+    var m = lookAt(vec3(0,0,-2.5), vec3(0.0,0.0,0.5), vec3(0,1,0));
+
+    var P = perspective(45, 1, 1, 5);
+
+    var Ry = rotateY(45);
+
+    var Rx = mult(rotateX(45), Ry);
+
+    var R0 = mat4();
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "view"), false, flatten(m));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "perspective"), false, flatten(P));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "rotation"), false, flatten(R0));
+
+    var cubeColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ], // black
+        [ 1.0, 0.0, 0.0, 1.0 ], // red
+        [ 1.0, 1.0, 0.0, 1.0 ], // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ], // green
+        [ 0.0, 0.0, 1.0, 1.0 ], // blue
+        [ 1.0, 0.0, 1.0, 1.0 ], // magenta
+        [ 1.0, 1.0, 1.0, 1.0 ], // white
+        [ 0.0, 1.0, 1.0, 1.0 ] // cyan
+    ];
+
+    var cubeVertices = [
+        vec4(-0.5, -0.5, 0.5, 1),
+        vec4(-0.5, 0.5, 0.5, 1),
+        vec4(0.5, 0.5, 0.5, 1),
+        vec4(0.5, -0.5, 0.5, 1),
+        vec4(-0.5, -0.5, -0.5, 1),
+        vec4(-0.5, 0.5, -0.5, 1),
+        vec4(0.5, 0.5, -0.5, 1),
+        vec4(0.5, -0.5, -0.5, 1)
+    ];
+
+    var wireIndices = new Uint32Array([
+        0, 1, 1, 2, 2, 3, 3, 0, // front
+        2, 3, 3, 7, 7, 6, 6, 2, // right
+        0, 3, 3, 7, 7, 4, 4, 0, // down
+        1, 2, 2, 6, 6, 5, 5, 1, // up
+        4, 5, 5, 6, 6, 7, 7, 4, // back
+        0, 1, 1, 5, 5, 4, 4, 0 // left
+    ]);
+    
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cubeVertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "a_Position");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(cubeColors), gl.STATIC_DRAW);
+
+    var cPosition = gl.getAttribLocation(program, "a_Color");
+    gl.vertexAttribPointer(cPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(cPosition);
+    
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);    
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(wireIndices), gl.STATIC_DRAW);
+    
+    function render() {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawElements(gl.LINES, numVertices, gl.UNSIGNED_BYTE, 0);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "rotation"), false, flatten(Ry));
+        gl.drawElements(gl.LINES, numVertices, gl.UNSIGNED_BYTE, 0);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "rotation"), false, flatten(Rx));
+        gl.drawElements(gl.LINES, numVertices, gl.UNSIGNED_BYTE, 0);    
+    }
+    
+    render();
+}   
